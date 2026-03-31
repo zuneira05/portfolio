70489ee8448715e82d1944a45be431c70c28dcbf
zuneira <zunairasayed20@gmail.com>
2026-03-31 22:44:50 +0530

    footer added

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ function toggleMenu(){
     icon.classList.toggle('open');
     console.log('clicked');
 }
+// Load time
+window.addEventListener("load", () => {
+  const time = performance.now().toFixed(0);
+  document.getElementById("loadTime").innerText =
+    `Loaded in ${time} ms ⚡`;
+});
+
+// Live clock
+function updateClock() {
+  const now = new Date();
+  document.getElementById("clock").innerText =
+    now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+// Console message (hidden)
+console.log("👀 Hey developer! Thanks for checking my portfolio.");
+
+// CTA click
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".cta span");
+
+  if (btn) {
+    btn.addEventListener("click", () => {
+      alert("Let's connect 😄");
+    });
+  }
+});
